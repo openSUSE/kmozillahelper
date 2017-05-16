c4b786334f2c9be05f2f2fb8e7b9fe4b4cea1d6b
Fabian Vogt <fabian@ritter-vogt.de>
2017-05-16 08:52:47 +0200

    Add workaround for setMainWindow causing firefox to crash with Qt >= 5.9
    Not great at all, but better than crashes. Needs further investigation.
    Workaround for issue #14

--- main.cpp
+++ main.cpp
@@ -48,7 +48,6 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include <KIOWidgets/KRun>
 #include <KNotifications/KNotification>
 #include <KService/KMimeTypeTrader>
-#include <KWindowSystem/KWindowSystem>
 
 //#define DEBUG_KDE
 
@@ -75,6 +74,32 @@ int main(int argc, char* argv[])
     app.installEventFilter(&helper);
 
     return app.exec();
+}
+
+/* Workaround for Qt 5.9 somehow crashing firefox if the QWindow::fromWinId result gets deleted.
+   Original code copied from KWindowSystems. */
+static void setMainWindow(QWidget *subWidget, WId mainWindowId)
+{
+    // Set the WA_NativeWindow attribute to force the creation of the QWindow.
+    // Without this QWidget::windowHandle() returns 0.
+    subWidget->setAttribute(Qt::WA_NativeWindow, true);
+    QWindow *subWindow = subWidget->windowHandle();
+    Q_ASSERT(subWindow);
+
+    QWindow *mainWindow = QWindow::fromWinId(mainWindowId);
+    if (!mainWindow) {
+        // foreign windows not supported on all platforms
+        return;
+    }
+
+    #if QT_VERSION < QT_VERSION_CHECK(5, 9, 0)
+        // mainWindow is not the child of any object, so make sure it gets deleted at some point
+        QObject::connect(subWidget, &QObject::destroyed, mainWindow, &QObject::deleteLater);
+    #else
+        #warning Qt 5.9.0 has a bug that/behavioural change that makes firefox crash. This workaround however causes memory leaks.
+    #endif
+
+    subWindow->setTransientParent(mainWindow);
 }
 
 Helper::Helper()
@@ -292,7 +317,7 @@ bool Helper::handleAppsDialog()
     dialog.hideNoCloseOnExit();
     dialog.hideRunInTerminal(); // TODO
     if(wid != 0)
-        KWindowSystem::setMainWindow(&dialog, wid);
+        setMainWindow(&dialog, wid);
     if(dialog.exec())
     {
         KService::Ptr service = dialog.service();
@@ -660,7 +685,7 @@ bool Helper::eventFilter(QObject *obj, QEvent *ev)
         QWidget *widget = static_cast<QWidget*>(obj);
         widget->winId();
         if(wid)
-            KWindowSystem::setMainWindow(widget, wid);
+            setMainWindow(widget, wid);
     }
 
     return false;
